4df747b72b1fefab99542b997d0733d1ea7af218
taidt21 <dinhtuantaiok@gmail.com>
2022-04-24 23:40:17 +0700

    product website
    product website-taidt21

---
+++ script.js
@@ -0,0 +1,39 @@
+$('.autoplay').slick({
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots:true,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          
+          
+        }
+      },
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite:true,
+          dots:false,
+        }
+      },
+     {
+       breakpoint:992,
+       settings:{
+         slidesToShow:1,
+         slidesToScroll:1,
+         infinite:true,
+         dots:false,
+       }
+     }
+    ]
+  });
